0bec850ce12fe56f31fed893e90eafee5fb53224
Dan Ahuv <s83532@ziv-school.com>
2023-12-30 19:19:09 +0200

    added comments and typings to operator and operand classes

--- OperandClasses.py
+++ OperandClasses.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 POWER_OF_OPERAND = 0
 POWER_OF_ADDITION = 1
 POWER_OF_SUBTRACTION = 1
@@ -10,20 +13,40 @@ POWER_OF_MAXIMUM = 5
 POWER_OF_MINIMUM = 5
 POWER_OF_NEGATIVE = 6
 POWER_OF_FACTORIAL = 6
+POWER_OF_PARENTHESIS = 7
 
 
 class EquationComponent(object):
-    def __init__(self, power_value: int):
+    def __init__(self, power_value: int) -> None:
+        """
+        The Function initialize an EquationComponent Class.
+        :param power_value: The power of the equation component.
+        The power indicates the order pf precedence in the expression.
+        """
         self.__power__ = power_value
 
 
 class Operand(EquationComponent):
-    def __init__(self, new_value):
+    def __init__(self, new_value: Union[int, float]) -> None:
+        """
+        The Function initialize an Operand.
+        The Function Sets The Operand Power to be Zero because it is not an operator.
+        :param new_value: the value of the operand.
+        """
         super().__init__(POWER_OF_OPERAND)
         self.__value__ = new_value
 
-    def get_value(self):
+    def get_value(self) -> Union[int, float]:
+        """
+        The function is a getter for the value of the operand.
+        :return: the value of the operand.
+        """
         return self.__value__
 
-    def set_value(self, new_value):
+    def set_value(self, new_value: Union[int, float]) -> None:
+        """
+        The function is a setter for the operand's value.
+        :param new_value: the parameter is the new value to set.
+        :return: None
+        """
         self.__value__ = new_value
--- OperatorClasses.py
+++ OperatorClasses.py
@@ -2,105 +2,233 @@ import OperandClasses
 
 
 class Operator(OperandClasses.EquationComponent):
-    def __init__(self, power_value):
+    def __init__(self, power_value: int) -> None:
+        """
+        The Function initialize an Operator Class.
+        :param power_value: The power of the Operator.
+        The power indicates the order pf precedence in the expression.
+        """
         super().__init__(power_value)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs a mathematical operation between two (or one) operands.
+        The Operator Function is general class so operation returns garbage value (0).
+        :param operand1: the first operand to perform the operator on.
+        :param operand2: the second operand to perform the operator on.
+        :return: 0
+        """
         return OperandClasses.Operand(0)
 
 
 class AdditionOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize an Addition Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_ADDITION)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs addition of two operands.
+        :param operand1: the first operand to perform the addition on.
+        :param operand2: the second operand to perform the addition on.
+        :return: the result of adding the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() + operand2.get_value())
 
 
 class SubtractionOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Subtraction Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_SUBTRACTION)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs subtraction of two operands.
+        :param operand1: the first operand to perform the subtraction on.
+        :param operand2: the second operand to perform the subtraction on.
+        :return: the result of subtracting the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() - operand2.get_value())
 
 
 class MultiplicationOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Multiplication Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_MULTIPLICATION)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs multiplication of two operands.
+        :param operand1: the first operand to perform the multiplication on.
+        :param operand2: the second operand to perform the multiplication on.
+        :return: the result of multiplying the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() * operand2.get_value())
 
 
 class DivisionOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Division Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_DIVISION)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs division of two operands.
+        The Function checks if the Second operand is equal to zero. in that case throw an exception.
+        :param operand1: the first operand to perform the division on.
+        :param operand2: the second operand to perform the division on.
+        :return: the result of dividing the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() / operand2.get_value())
 
 
 class PowerOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Power Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_POWER)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs powering of two operands.
+        The Function checks if the Second operand and the first operand are equal to zero,
+        in that case throw an exception.
+        :param operand1: the first operand to perform the powering on.
+        :param operand2: the second operand to perform the powering on.
+        :return: the result of powering the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() ** operand2.get_value())
 
 
 class ModuluOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Modulu Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_MODULU)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function performs modulu of two operands.
+        The Function checks if the Second operand is equal to zero, in that case throw an exception.
+        :param operand1: the first operand to perform the modulu on.
+        :param operand2: the second operand to perform the modulu on.
+        :return: the result of modulu the 2 operands.
+        """
         return OperandClasses.Operand(operand1.get_value() % operand2.get_value())
 
 
 class AverageOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize an Average between two operands Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_AVERAGE)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
-        return OperandClasses.Operand(OperandClasses.Operand((operand1.get_value() + operand2.get_value()) / 2))
+        """
+        The Function returns the average of two operands.
+        :param operand1: the first operand to calculate the average from.
+        :param operand2: the second operand to calculate the average from.
+        :return: the average of the operands.
+        """
+        return OperandClasses.Operand((operand1.get_value() + operand2.get_value()) / 2)
 
 
 class MaximumOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Maximum between two operands Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_MAXIMUM)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function returns the bigger operand between the two operands.
+        :param operand1: the first operand to check if bigger.
+        :param operand2: the second operand to check if bigger.
+        :return: the bigger number between the two.
+        """
         if operand1 >= operand2:
             return operand1
         return operand2
 
 
 class MinimumOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Minimum between two operands Operator Class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_MINIMUM)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand) -> OperandClasses.Operand:
+        """
+        The Function returns the smaller operand between the two operands.
+        :param operand1: the first operand to check if smaller.
+        :param operand2: the second operand to check if smaller.
+        :return: the smaller number between the two.
+        """
         if operand1 <= operand2:
             return operand1
         return operand2
 
 
 class NegativeOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a Negative of an operand class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_NEGATIVE)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand = 0) \
             -> OperandClasses.Operand:
+        """
+        The Function returns the negative value of operand1.
+        :param operand1: the operand to negative.
+        :param operand2: the second operand is useless. by default, it equals to 0.
+        :return: the negative of operand1.
+        """
         return OperandClasses.Operand(operand1.get_value() * -1)
 
 
 class FactorialOperator(Operator):
-    def __init(self):
+    def __init(self) -> None:
+        """
+        The Function initialize a factorial of an operand class.
+        :return: None
+        """
         super().__init__(OperandClasses.POWER_OF_FACTORIAL)
 
     def operation(self, operand1: OperandClasses.Operand, operand2: OperandClasses.Operand = 0)\
             -> OperandClasses.Operand:
-        factorial = 0
-        for i in range(1, operand1.get_value() + 1):
-            factorial = factorial * i
+        """
+        The Function returns the factorial value of operand1.
+        The function will check if operand1 is positive (or zero) and Integer.
+        If not, the function will throw an Exception.
+        :param operand1: the operand to calculate the factorial of.
+        :param operand2: the second operand is useless. by default, it equals to 0.
+        :return: the negative of operand1.
+        """
+        factorial = 1
+        for index in range(1, operand1.get_value() + 1):
+            factorial = factorial * index
         return OperandClasses.Operand(factorial)
